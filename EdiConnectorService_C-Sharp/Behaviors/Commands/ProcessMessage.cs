b21ba58174eaa07f3cce5be3cbe5a28060df68f4
Sharif Bayoumy <sharif.bayoumy@swsolutions.nl>
2017-08-01 16:46:56 +0200

    [ADDS] SAP update method to update the status of incoming xml messages

--- EdiConnectorService_C-Sharp/Behaviors/Commands/ProcessMessage.cs
+++ EdiConnectorService_C-Sharp/Behaviors/Commands/ProcessMessage.cs
@@ -11,7 +11,7 @@ namespace EdiConnectorService_C_Sharp
         string filePath;
         string fileName;
         string connectedServer;
-        string userTableCode;
+        string recordCode;
 
         public ProcessMessage(string _connectedServer, string _filePath, string _fileName)
         {
@@ -22,7 +22,7 @@ namespace EdiConnectorService_C_Sharp
 
         public void execute()
         {
-            userTableCode = AddIncomingXmlMessage(connectedServer, filePath, fileName, "Processing...", "", DateTime.Now);
+            recordCode = AddIncomingXmlMessage(connectedServer, filePath, fileName, "Processing..", "", DateTime.Now);
             XDocument xDoc = XDocument.Load(filePath + fileName);
             XElement xMessages = xDoc.Element("Messages");
             EdiDocument ediDocument = new EdiDocument();
@@ -41,6 +41,7 @@ namespace EdiConnectorService_C_Sharp
             {
                 ediDocument.SetDocumentType(new InvoiceDocument());
             }
+            UpdateIncomingXmlMessage(connectedServer, recordCode, "Processing..", "Set document type to: " + ediDocument.GetDocumentType().ToString());
 
             // Reads the XML Data for the specified document type
             ediDocumentData = ediDocument.ReadXMLData(xMessages);
@@ -53,6 +54,7 @@ namespace EdiConnectorService_C_Sharp
         {
             SAPbobsCOM.UserTable oUDT;
             oUDT = ConnectionManager.getInstance().GetConnection(_connectedServer).Company.UserTables.Item("0_SWS_EDI");
+            SAPbobsCOM.Recordset oRs = (SAPbobsCOM.Recordset)(ConnectionManager.getInstance().GetConnection(_connectedServer).Company.GetBusinessObject(SAPbobsCOM.BoObjectTypes.BoRecordset));
 
             try
             {
@@ -64,29 +66,47 @@ namespace EdiConnectorService_C_Sharp
 
                 if (oUDT.Add() == 0)
                 {
-                    EventLogger.getInstance().EventInfo("Succesfully added incoming xml message to UDT: " + oUDT.Name);
-                    return oUDT.Code;
+                    oRs.DoQuery(@"SELECT Max(""Code"") FROM ""@0_SWS_EDI""");
+                    recordCode = oRs.Fields.Item(0).Value.ToString();
+                    EventLogger.getInstance().EventInfo("Succesfully added incoming xml message to UDT: " + oUDT.TableName);
+                    return recordCode;
                 }
                 else
                 {
                     EventLogger.getInstance().EventError("Error adding items to UDT: " + ConnectionManager.getInstance().GetConnection(_connectedServer).Company.GetLastErrorDescription());
-                    return null;
+                    return recordCode = null;
                 }
             }
             catch
             {
                 EventLogger.getInstance().EventError("Error adding items to UDF: " + ConnectionManager.getInstance().GetConnection(_connectedServer).Company.GetLastErrorDescription());
-                return null;
+                return recordCode = null;
             }
             finally
             {
                 EdiConnectorService.ClearObject(oUDT);
+                EdiConnectorService.ClearObject(oRs);
             }
         }
 
-        private void UpdateIncomingXmlMessage()
+        private void UpdateIncomingXmlMessage(string _connectedServer, string _recordCode, string _status, string _logMessage)
         {
+            SAPbobsCOM.UserTable oUDT;
+            oUDT = ConnectionManager.getInstance().GetConnection(_connectedServer).Company.UserTables.Item("0_SWS_EDI");
 
+            oUDT.GetByKey(_recordCode);
+
+            oUDT.UserFields.Fields.Item("U_STATUS").Value = _status;
+            oUDT.UserFields.Fields.Item("U_LOG_MESSAGE").Value = _logMessage;
+
+            if (oUDT.Update() == 0)
+            {
+                EventLogger.getInstance().EventInfo("Succesfully updated incoming xml message to UDT: " + oUDT.TableName);
+            }
+            else
+            {
+                EventLogger.getInstance().EventError("Error updating items to UDT: " + ConnectionManager.getInstance().GetConnection(_connectedServer).Company.GetLastErrorDescription());
+            }
         }
     }
 }
