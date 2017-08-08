88f8b6692b87a62eb1cfb23f012121d9b7cb82d4
Sharif Bayoumy <sharif.bayoumy@swsolutions.nl>
2017-08-08 12:37:59 +0200

    [ADDS] Function to only add connections that has every field filled in

--- EdiConnectorService_C-Sharp/Behaviors/Commands/CreateConnectionsCommand.cs
+++ EdiConnectorService_C-Sharp/Behaviors/Commands/CreateConnectionsCommand.cs
@@ -20,8 +20,22 @@ namespace EdiConnectorService_C_Sharp
             XDocument xDoc = XDocument.Load(EdiConnectorData.getInstance().sApplicationPath + @"config.xml");
             foreach (XElement xEle in xDoc.Element("Connections").Elements("Connection"))
             {
-                ConnectionManager.getInstance().Connections.Add(xEle.Element("Server").Value, new SAPConnection());
-                ConnectionManager.getInstance().Connections.Last().Value.Set(xEle);
+                bool emptyFieldFound = false;
+                foreach(XElement xChild in xEle.Descendants())
+                {
+                    if(xChild.Value == "")
+                    {
+                        emptyFieldFound = true;
+                        break;
+                    }
+                }
+                if (!emptyFieldFound)
+                {
+                    ConnectionManager.getInstance().Connections.Add(xEle.Element("Server").Value, new SAPConnection());
+                    ConnectionManager.getInstance().Connections.Last().Value.Set(xEle);
+                }
+                else
+                    EventLogger.getInstance().EventError("Error creating connections: Empty field found in configuration " + xEle.Element("Server"));
             }
         }
     }
